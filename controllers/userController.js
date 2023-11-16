f83df5a4ae1b0bf1f927d7a438b68ab59146355e
Raja krishna Maurya <krishna.raja78@gmail.com>
2023-11-16 21:47:59 +0530

    feat: code refatoring code using async handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,92 +10,87 @@ exports.getUsers = asyncHandler(async (req, res, next) => {
   let content = JSON.parse(dataRead);
 
   if (content) {
-    return res.status(200).send(content);
+    return res.status(200).json({
+      success: true,
+      count: content.length,
+      data: content,
+      msg: `users fetched`,
+    });
   }
   next(new ErrorResponse("Bad request. Please try again..", 400));
 });
 // get single user
-exports.getUser = async (req, res, next) => {
-  try {
-    const dataRead = await fs.readFile("post.json", "utf-8");
-    let record = JSON.parse(dataRead);
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const dataRead = await fs.readFile("post.json", "utf-8");
+  let record = JSON.parse(dataRead);
 
-    record = record.find((obj) => obj["id"] == req.params.id);
+  record = record.find((obj) => obj["id"] == req.params.id);
 
-    if (record) {
-      return res.status(200).json({ success: true, data: record });
-    } else {
-      next(new ErrorResponse("No record found.."), 200);
-    }
-  } catch (err) {
-    res.status(400).json({ success: false, data: [] });
-    next(err);
+  if (record) {
+    return res.status(200).json({
+      success: true,
+      data: record,
+    });
+  } else {
+    next(new ErrorResponse("No record found.."), 200);
   }
-};
+
+  next(new ErrorResponse("Internal server error. Please try again..", 500));
+});
 
 // create a user
-exports.addUser = async (req, res, next) => {
-  try {
-    let content = null;
-    let rawData = await fs.readFile("post.json", "utf-8");
-    rawData = rawData != "" ? JSON.parse(rawData) : null;
-    if (rawData && Array.isArray(rawData)) {
-      rawData.push(req.body);
-      content = stringify(rawData);
-    } else {
-      content = stringify([req.body]);
-    }
-
-    await fs.writeFile("post.json", content, "utf-8");
-    res.status(200).json({
-      success: true,
-      count: JSON.parse(content).length,
-      data: JSON.parse(content),
-      msg: `new user has been added successfully`,
-    });
-  } catch (err) {
-    next(err);
+exports.addUser = asyncHandler(async (req, res, next) => {
+  let content = null;
+  let rawData = await fs.readFile("post.json", "utf-8");
+  rawData = rawData != "" ? JSON.parse(rawData) : null;
+  if (rawData && Array.isArray(rawData)) {
+    rawData.push(req.body);
+    content = stringify(rawData);
+  } else {
+    content = stringify([req.body]);
   }
-};
+
+  await fs.writeFile("post.json", content, "utf-8");
+  res.status(200).json({
+    success: true,
+    count: JSON.parse(content).length,
+    data: JSON.parse(content),
+    msg: `new user has been added successfully`,
+  });
+  next(new ErrorResponse("Couldn't create new user. Please try again..", 500));
+});
 // update a user
-exports.updateUser = async (req, res, next) => {
-  try {
-    const dataRead = await fs.readFile("post.json", "utf-8");
-    let records = JSON.parse(dataRead);
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const dataRead = await fs.readFile("post.json", "utf-8");
+  let records = JSON.parse(dataRead);
 
-    records.forEach((record) => {
-      if (record.id == req.params.id) {
-        const { name } = req.body;
-        record.name = name;
-      }
-    });
-    if (records) {
-      await fs.writeFile("post.json", JSON.stringify(records), "utf-8");
-      return res.status(200).json({ success: true, data: records });
+  records.forEach((record) => {
+    if (record.id == req.params.id) {
+      const { name } = req.body;
+      record.name = name;
     }
-  } catch (err) {
-    res.status(400).json({ success: false, data: [] });
-    next(err);
+  });
+  if (records) {
+    await fs.writeFile("post.json", JSON.stringify(records), "utf-8");
+    return res.status(200).json({ success: true, data: records });
   }
-};
+
+  next(new ErrorResponse("Please try again..", 500));
+});
 
 // delete a user
-exports.deleteUser = async (req, res, next) => {
-  try {
-    const dataRead = await fs.readFile("post.json", "utf-8");
-    let record = JSON.parse(dataRead);
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  const dataRead = await fs.readFile("post.json", "utf-8");
+  let record = JSON.parse(dataRead);
 
-    let index = record.findIndex((obj) => obj["id"] == req.params.id);
-    record.splice(index, 1);
+  let index = record.findIndex((obj) => obj["id"] == req.params.id);
+  record.splice(index, 1);
 
-    if (record) {
-      await fs.writeFile("post.json", JSON.stringify(record), "utf-8");
-      return res
-        .status(200)
-        .json({ success: true, count: record.length, data: record });
-    }
-  } catch (err) {
-    res.status(400).json({ success: false, data: [] });
-    next(err);
+  if (record) {
+    await fs.writeFile("post.json", JSON.stringify(record), "utf-8");
+    return res
+      .status(200)
+      .json({ success: true, count: record.length, data: record });
   }
-};
+  next(new ErrorResponse("having problem in delete.. try again later", 500));
+});
